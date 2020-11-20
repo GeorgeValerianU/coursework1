8dad8638ed73a6dcd4ed28bb7b5dac2bf097a5ac
George Ursache <gu038@live.mdx.ac.uk>
2020-11-20 11:16:29 +0000

    Added the list of lessons to the lessons.js

---
+++ lessons.js
@@ -0,0 +1,72 @@
+let products = [
+    {
+        "id": 101,
+        "subject": "Math",
+        "location": "London",
+        "price": 100,
+        "spaces": 5
+    },
+    {
+        "id": 102,
+        "subject": "Math",
+        "location": "York",
+        "price": 80,
+        "spaces": 5
+    },
+    {
+        "id": 103,
+        "subject": "Math",
+        "location": "Oxford",
+        "price": 85,
+        "spaces": 5
+    },
+    {
+        "id": 104,
+        "subject": "English",
+        "location": "London",
+        "price": 120,
+        "spaces": 5
+    },
+    {
+        "id": 105,
+        "subject": "English",
+        "location": "Bristol",
+        "price": 70,
+        "spaces": 5
+    },
+    {
+        "id": 106,
+        "subject": "Music",
+        "location": "London",
+        "price": 115,
+        "spaces": 5
+    },
+    {
+        "id": 107,
+        "subject": "Music",
+        "location": "Cardiff",
+        "price": 60,
+        "spaces": 5
+    },
+    {
+        "id": 108,
+        "subject": "Science",
+        "location": "London",
+        "price": 95,
+        "spaces": 5
+    },
+    {
+        "id": 109,
+        "subject": "Science",
+        "location": "Birmingham",
+        "price": 65,
+        "spaces": 5
+    },
+    {
+        "id": 110,
+        "subject": "Science",
+        "location": "Liverpool",
+        "price": 50,
+        "spaces": 5
+    },
+]
